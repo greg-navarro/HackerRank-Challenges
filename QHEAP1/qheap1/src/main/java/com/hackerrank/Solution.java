aa6c587cc529faaa1dc8c74ea97facb26a5e2333
Gregory Litman-Navarro <gregorylitmannavarro@gmail.com>
2022-02-06 10:49:36 -0800

    start the QHEAP1 question

---
+++ QHEAP1/qheap1/src/main/java/com/hackerrank/Solution.java
@@ -0,0 +1,17 @@
+package com.hackerrank;
+
+/**
+ * Hello world!
+ */
+public final class Solution {
+    private Solution() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
