a3c36b78a73fb42d3ed29a4d9dd112f90beb0d50
Gregory Litman-Navarro <gregorylitmannavarro@gmail.com>
2022-03-24 10:19:56 -0700

    Try slightly smarter approach

--- challenges/src/SumVsXor.java
+++ challenges/src/SumVsXor.java
@@ -1,14 +1,21 @@
 public class SumVsXor {
     public static long sumXor(long n) {
         // Write your code here
-        long result = 0;
-        long operand = 0;
-        while (operand < n) {
-            long sum = n + operand;
-            long exor = n ^ operand;
-            if (sum == exor) result++;
-            operand++; 
+        long zeros = 0;
+        // convert n to a binary string
+        String nstr = Long.toString(n, 2);
+        // find index of first 1
+        int index = -1;
+        for (int i = 0; i < nstr.length() && index == -1; i++) {
+            if (nstr.charAt(i) == '1') index = i;
         }
+        //  starting with the index we just identified (if found)
+        //  iterate through the string and for each 0 found increase result by one
+        while (index < nstr.length()) {
+            if (nstr.charAt(index) == '0') zeros++;
+            index++;
+        }
+        long result = Math.pow(2, 1 + zeros);
         return result;
     }
 
